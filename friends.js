6fde58ab8596c3080d6b5bb6f2a9b36c42e6ad0c
Ben Luerding <bluerding@gmail.com>
2022-11-09 17:35:20 -0600

    Add good friend

--- friends.js
+++ friends.js
@@ -1,7 +1,10 @@
 var bestFriend = "Joe"
+var goodFriend = "Matt"
 
 if (bestFriend === "Ben"){
     console.log("Hey buddy!")
+} else if (goodFriend === "Matt"){
+    console.log("Hey Matt!")
 } else {
     console.log("Hey")
 }
